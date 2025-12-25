869d4cc33617bd416475b3f72937324449141ae2
sourav1916 <souravadhikary1916@gmail.com>
2025-12-25 14:27:30 +0530

    Bank create and list api done

--- helpers/function.js
+++ helpers/function.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import pool from "../db.js";
 
 const RANDOM_STRING = (length = 30) => {
@@ -26,6 +27,10 @@ const RANDOM_INTEGER = (length = 6) => {
     const max = Math.pow(10, length) - 1;
 
     return Math.floor(min + Math.random() * (max - min + 1));
+}
+
+const TODAY_DATE = () => {
+    return moment().format("YYYY-MM-DD")
 }
 
 function GENERATE_PASSWORD(length = 8) {
@@ -68,10 +73,96 @@ async function USER_DATA(username = '') {
 }
 
 
+async function SET_OPENING_BALANCE({
+    req = {},
+    type = "0",
+    party1_type = "",
+    party1_id = "",
+    amount = 0,
+    remark = "",
+    transaction_date = moment().format("YYYY-MM-DD")
+}) {
+    const username = req?.headers["username"] || "";
+    const branch_id = req?.headers["branch_id"] || "";
+
+    const connection = await pool.getConnection();
+
+    try {
+        await connection.beginTransaction();
+
+        const transaction_id = RANDOM_STRING(30);
+        const invoice_id = RANDOM_STRING(30);
+
+        // CHECK INVOICE PREFIX (IF not exist then throw error)
+
+        const [invoice_prefix] = await connection.query("SELECT * FROM `invoice_prefix` WHERE `branch_id` = ? AND `type` = ? AND `status` = ? AND `issue_date` <= ? AND `expire_date` >= ?", [branch_id, "opening balance", "1", TODAY_DATE(), TODAY_DATE()]);
+
+        if (invoice_prefix.length == 0) {
+            throw new Error("Invoice prefix not set.");
+        }
+
+        const invoice_data = invoice_prefix[0];
+        const invoice_primary_id = invoice_data?.id;
+        const serial = Number(invoice_data?.current || 0) + 1;
+
+        const invoice_no = `${invoice_data?.prefix}${serial}`
+
+
+        await connection.query("INSERT INTO `invoice` (`invoice_id`, `branch_id`, `invoice_no`, `create_by`, `modify_by`, `type`, `transaction_id`, `party1_type`, `party1_id`, `amount`, `total`, `remark`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", [invoice_id, branch_id, invoice_no, username, username, "opening balance", transaction_id, party1_type, party1_id, amount, amount, remark]);
+
+        await connection.query("INSERT INTO `transactions` (`branch_id`, `transaction_id`, `create_by`, `modify_by`, `transaction_date`, `amount`, `type`, `transaction_type`, `invoice_id`, `invoice_no`, `party1_type`, `party1_id`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", [branch_id, transaction_id, username, username, transaction_date, amount, type, "opening balance", invoice_id, invoice_no, party1_type, party1_id]);
+
+        await connection.query("UPDATE `invoice_prefix` SET `current`= ? WHERE `id` = ?", [serial, invoice_primary_id]);
+
+        await connection.commit();
+        return true;
+
+    } catch (error) {
+        await connection.rollback();
+        throw error;
+    } finally {
+        connection.release();
+    }
+}
+
+async function GET_BALANCE({
+    branch_id = "",
+    party_id = "",
+    party_type = ""
+}) {
+
+    const connection = await pool.getConnection();
+
+    try {
+        await connection.beginTransaction();
+
+        const [total_0] = await connection.query("SELECT SUM(amount) AS value FROM transactions WHERE branch_id = ? AND type = ? AND party1_type = ? AND party1_id = ?", [branch_id, '0', party_type, party_id]);
+        const debit = Number(total_0[0]?.value || 0);
+
+        const [total_1] = await connection.query("SELECT SUM(amount) AS value FROM transactions WHERE branch_id = ? AND type = ? AND party1_type = ? AND party1_id = ?", [branch_id, '1', party_type, party_id]);
+        const credit = Number(total_1[0]?.value || 0);
+
+        const balance = debit - credit;
+
+        await connection.commit();
+        return balance;
+
+    } catch (error) {
+        await connection.rollback();
+        throw error;
+    } finally {
+        connection.release();
+    }
+}
+
+
 export {
     RANDOM_STRING,
     GENERATE_PASSWORD,
     IS_STRONG_PASSWORD,
     USER_DATA,
-    RANDOM_INTEGER
+    RANDOM_INTEGER,
+    SET_OPENING_BALANCE,
+    TODAY_DATE,
+    GET_BALANCE
 };
--- routes/transactions/transactions.routes.js
+++ routes/transactions/transactions.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import pool from "../../db.js";
 import { auth } from "../../middleware/auth.js";
-import { RANDOM_STRING } from "../../helpers/function.js";
+import { GET_BALANCE, RANDOM_STRING, SET_OPENING_BALANCE } from "../../helpers/function.js";
 
 const router = express.Router();
 
@@ -55,7 +55,8 @@ router.post("/banks/create", auth, async (req, res) => {
             bank,
             branch,
             type,
-            remark
+            remark,
+            opening_balance = {}
         } = req.body || {};
 
 
@@ -78,6 +79,21 @@ router.post("/banks/create", auth, async (req, res) => {
         );
 
 
+        const amount = opening_balance?.amount;
+        const transaction_date = opening_balance?.date;
+        const transaction_type = opening_balance?.type;
+
+        await SET_OPENING_BALANCE({
+            req,
+            type: transaction_type,
+            party1_type: "bank",
+            party1_id: bank_id,
+            amount,
+            remark: "",
+            transaction_date
+        })
+
+
 
 
         return res.status(200).json({
@@ -88,6 +104,77 @@ router.post("/banks/create", auth, async (req, res) => {
     } catch (error) {
         console.error("Create bank fatal error:", error);
         return res.status(500).json({ success: false, message: "Failed to create bank", error: error.message });
+    }
+});
+
+router.get('/banks/list', auth, async (req, res) => {
+    try {
+        const username = req.headers["username"] || "";
+        const branch_id = req.headers["branch_id"] || "";
+        const { page_no = 1, query = "" } = req?.body || {};
+
+        const limit = 20;
+        const offset = (page_no - 1) * limit;
+        const search_sql = `%${query}%`;
+
+        const [rows] = await pool.query(
+            "SELECT * FROM banks WHERE branch_id = ? AND (account_no LIKE ? OR holder LIKE ? OR ifsc LIKE ? OR bank LIKE ? OR branch LIKE ? OR remark LIKE ?) LIMIT ? OFFSET ?",
+            [branch_id, search_sql, search_sql, search_sql, search_sql, search_sql, search_sql, limit, offset]
+        );
+
+
+        const bank_list = [];
+
+        for (let index = 0; index < rows.length; index++) {
+            const element = rows[index];
+
+            const bank_id = element?.bank_id;
+            const account_no = element?.account_no;
+            const ifsc = element?.ifsc;
+            const holder = element?.holder;
+            const remark = element?.remark;
+            const bank = element?.bank;
+            const branch = element?.branch;
+            const type = element?.type;
+            const status = element?.status == '1' ? true : false;
+
+            const balance = await GET_BALANCE({ party_type: "bank", party_id: bank_id, branch_id })
+
+
+            const object = {
+                bank_id,
+                account_no,
+                ifsc,
+                holder,
+                remark,
+                bank,
+                branch,
+                status,
+                type,
+                balance
+            }
+
+
+
+
+            bank_list.push(object);
+
+
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: bank_list,
+            meta: {
+                page_no,
+                total: bank_list.length
+            }
+        })
+
+
+    } catch (error) {
+        console.error('Error fetching bank list:', error);
+        return res.status(500).json({ success: false, message: "Failed to fetch bank list", error: error.message });
     }
 });
 
